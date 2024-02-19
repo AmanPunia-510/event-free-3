7be72a5d38958899f9efb5b70b5963f154a7239b
AmanPunia-510 <puniaaman888@gmail.com>
2024-02-19 15:47:20 +0530

    change in some classes

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menubar");
-let nav = document.querySelector(".nav-items")
-
-menu.addEventListener("click", function () {
-    nav.classList.toggle("show")
-})
-
